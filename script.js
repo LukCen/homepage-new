d12e6e4c67cb47e4e08cc6c92eb0ef8d7777f54e
LBJ <lbjcena@gmail.com>
2024-03-05 07:58:22 +0100

    basic dropdown switch logic added

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const menuContainer = document.querySelector('.nav-button-container')
 const btnMenuMobile = document.querySelector('#menu-mobile')
 
-btnMenuMobile.addEventListener('click', () => {
+const closeMenu = (event) => {
+  menuContainer.classList.remove('active')
+}
+
+btnMenuMobile.addEventListener('touchstart', (e) => {
   menuContainer.classList.toggle('active')
+  e.stopPropagation()
 })
 
+document.addEventListener('touchstart', (e) => {
+  if (menuContainer.classList.contains('active')) {
+    closeMenu(e)
+  }
+})
